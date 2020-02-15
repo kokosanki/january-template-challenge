468eca62edc4c042844cd1612915a90a271058d3
kokosanki <os_mo@wp.pl>
2020-02-15 17:38:40 +0100

    code fixes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,33 @@
 import '../styles/index.scss';
+
+import Slider from './slider';
+import Quotation from './quotation';
+import Works from './works';
+import Modal from './modal';
+import Recommendation from './recommendation';
+import Nav from './nav';
+import Hamburger from './hamburger';
+
+class App {
+    constructor() {
+        this.slider = new Slider();
+        this.quotation = new Quotation();
+        this.works = new Works();
+        this.modal = new Modal();
+        this.recommendation = new Recommendation();
+        this.nav = new Nav();
+        this.hamburger = new Hamburger();
+    }
+
+    init() {
+        this.slider.init();
+        this.quotation.init();
+        this.works.init();
+        this.modal.init();
+        this.recommendation.init();
+        this.nav.init();
+        this.hamburger.init();
+    }
+}
+
+export default App;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,10 @@
 import '../styles/index.scss';
 
-import Slider from './slider';
-import Quotation from './quotation';
-import Works from './works';
-import Modal from './modal';
-import Recommendation from './recommendation';
-import Nav from './nav';
-import Hamburger from './hamburger';
-
+import App from './app';
 
 function startApp() {
-    const slider = new Slider();
-    slider.init();
-    const quotation = new Quotation();
-    quotation.init();
-    const works = new Works();
-    works.init();
-    const modal = new Modal();
-    modal.init();
-    const recommendation = new Recommendation();
-    recommendation.init();
-    const nav = new Nav();
-    nav.init();
-    const hamburger = new Hamburger();
-    hamburger.init();
+    const app = new App();
+    app.init();
 }
 
 startApp();
